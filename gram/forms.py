3a0d5f27c1184967d8bc8111bbea00c151a1e9db
Nyash-Mauro <mauricee423@gmail.com>
2020-11-22 03:49:39 +0300

    added forms to upload photo

--- gram/forms.py
+++ gram/forms.py
@@ -1,22 +1,22 @@
 from django import forms
-from . models import Photo,Profile,Comments
-from django.forms import ModelForm      
+from . models import Photo, Profile, Comments
+from django.forms import ModelForm
 from .models import Photo
 
 
 class PhotoForm(ModelForm):
     class Meta:
         model = Photo
-        exclude = ['profile','likes','comments','user']
-        
+        exclude = ['profile', 'likes', 'comments', 'user']
+
+
 class ImageProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ['user',]
+        exclude = ['user', ]
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comments
-        exclude = ['user','image','date_posted']
-    
-        
+        exclude = ['user', 'image', 'date_posted']
