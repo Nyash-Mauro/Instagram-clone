2e2943f7197c70937e222ef66a1d78bf49183feb
Nyash-Mauro <mauricee423@gmail.com>
2020-11-22 04:48:02 +0300

    upgraded corsheaders to fix six error

--- instagram/settings.py
+++ instagram/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tinymce',
+    'corsheaders',
     'vote',
     'gram',
     'cloudinary',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
